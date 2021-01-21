4ee09550be64e336c937ae5d31892fb6885fd841
summerlightningman <div8@bk.ru>
2021-01-21 14:20:09 +0300

    Added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const githubLink = 'https://github.com/summerlightningman';
+
+    return (
+        <Container>
+            <h1>My Trello Analogue</h1>
+            <h6>For my <a href={githubLink}>GitHub-repository</a></h6>
+            <hr/>
+        </Container>
+    );
 }
 
 export default App;
